02c49b99d35f1f815baa8dc766186610defa27e0
fernandopadangg <michael.fernando@ottencoffee.co.id>
2021-02-21 12:40:24 +0700

    add new component loader

---
+++ packages/components/src/loader/index.tsx
@@ -0,0 +1,9 @@
+import { Loading } from './style';
+
+const Loader = () => {
+  return (
+    <Loading src="https://upload.wikimedia.org/wikipedia/commons/c/c7/Loading_2.gif" alt="Loading GIF" />
+  )
+}
+
+export default Loader;
