5e9145557de356cd28425192527e216bcabf66bd
fernandopadangg <michael.fernando@ottencoffee.co.id>
2021-02-21 13:30:52 +0700

    update _app.tsx provider from stores package (use redux)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
-
+import Provider from '@web-planet/stores';
+import '../public/css/app.css'
 
 const MyApp = (props: { Component: any, pageProps: any }) =>{
   const { Component, pageProps } = props;
-  return <Component {...pageProps} />
+  return <Provider><Component {...pageProps} /></Provider>
 }
 
-export default MyApp;
+export default MyApp
